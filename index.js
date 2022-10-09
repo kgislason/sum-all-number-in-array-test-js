04f2a8492701064f5ef6264a04f14305c2df24a6
Kristy Gislason <kristygislason@gmail.com>
2022-10-09 13:14:27 -0700

    Implement recursion summing array

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let result = 0;
+  if (array.length === 0) {
+    result += 0;
+  } else if (! Array.isArray(array)) {
+    return "Error: not an array";
+  } else {
+    for (let item of array) {
+      if (! Array.isArray(item)) {
+        result += Number(item);
+      } else {
+        result += Number(sumItems(item));
+      }
+    }
+  }
+
+  return result;
+};
+
+console.log(sumItems([ 1, [2, 3, [4, 5]], 6]));
 
 module.exports = sumItems;
